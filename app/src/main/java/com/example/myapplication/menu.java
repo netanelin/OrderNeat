d21eedf7036b21689f8c39ac725e855c52ace76f
netanelin <netanel117@gmail.com>
2020-01-13 23:52:29 +0200

    Update menu.java

--- app/src/main/java/com/example/myapplication/menu.java
+++ app/src/main/java/com/example/myapplication/menu.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -32,79 +34,48 @@ public class menu extends AppCompatActivity {
         listViewProducts = (ListView) findViewById(R.id.listViewProducts);
         productsList = new ArrayList<>();
         db = FirebaseFirestore.getInstance();
-
-//        Map<String, Object> m1 = new HashMap<>();
-//        m1.put("name", "Humburger");
-//        m1.put("description", "Atomic Hum");
-//        m1.put("price", 32);
-//
-//        Map<String, Object> m2 = new HashMap<>();
-//        m1.put("name", "Sandwich");
-//        m1.put("description", "Good vibes Sandwich");
-//        m1.put("price", 25);
-//
-//        Map<String, Object> m3 = new HashMap<>();
-//        m1.put("name", "Salad");
-//        m1.put("description", "Refreshing israely Salad");
-//        m1.put("price", 19);
-//
-//        Vector<Map> productsList = new Vector<>();
-//        productsList.add(m1);
-//        productsList.add(m2);
-//        productsList.add(m3);
-//
-//        Map<String, Object> productsList[] = new Map[]{m1, m2, m3};
-//
-//        ListView listview = findViewById(R.id.lv_products_list);
-//        ArrayAdapter<Map<String, Object>> adapter = new ArrayAdapter<Map<String, Object>>(this, android.R.layout.simple_list_item_1, productsList);
-//        listview.setAdapter(adapter);
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        ///--------debug--------
-        Toast.makeText(menu.this,"debugging: inside menu::onStart", Toast.LENGTH_LONG);
-        ///--------debug--------
-
         db.collection("Products")
-                //.whereEqualTo("available", true)//
+                .whereEqualTo("available", true)
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots,
                                         @Nullable FirebaseFirestoreException e) {
 
-                        ///--------debug--------
-                        Toast.makeText(menu.this,"debugging: inside menu::onEvent", Toast.LENGTH_LONG);
-                        ///--------debug--------
-
                         if (e != null) {
                             Log.w(TAG, "Listen to products list failed.", e);
-
-                            ///--------debug--------
-                            Toast.makeText(menu.this,"error: Listen to products list failed.", Toast.LENGTH_LONG);
-                            ///--------debug--------
-
                             return;
                         }
+                        //empty products list before getting the new one from firestore
                         productsList.clear();
+                        //for each document in the collection
                         for (QueryDocumentSnapshot productSnapshot : queryDocumentSnapshots)
-                            if((boolean)productSnapshot.get("available") == true)
+                            //if the product is available
+                           // if((boolean)productSnapshot.get("available") == true)
+                                //add the product to the list as a Map<String, Object>
                                 productsList.add(productSnapshot.getData());
                         Log.d(TAG, "Current available products: " + productsList);
-                        //creating new adapter
+                        //creating new adapter and giving him this activity as context and the current list of products
                         ProductsList adapter = new ProductsList(menu.this, productsList);
-
-                        ///--------debug--------
-                        System.out.println(productsList);
-                        Toast.makeText(menu.this,"we have got products list from firestore", Toast.LENGTH_LONG);
-                        ///--------debug--------
 
                         //giving the adapter to the listView
                         listViewProducts.setAdapter(adapter);
                     }
                 });
+
+        listViewProducts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get the selected item Map object
+                Map<String, Object> selectedItem = (Map<String, Object>) parent.getItemAtPosition(position);
+
+
+            }
+        });
     }
 }
